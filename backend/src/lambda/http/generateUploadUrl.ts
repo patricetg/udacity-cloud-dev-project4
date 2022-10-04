3f0fb878ac5f317b3383a7f9092ff4c2614b6c05
Ekou LOGOSU-TEKO <patricelogosuteko@gmail.com>
2022-10-04 13:54:01 +0000

    generateUploadUrl implemented

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -4,16 +4,22 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
+import { createAttachmentPresignedUrl } from '../../layers/business/todos'
 import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
-    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id - DONE
+    const url = await createAttachmentPresignedUrl(todoId);
+    console.log("url : "+ url);
 
-    return undefined
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        attachmentUrl: url
+      })
+    };
   }
 )
 
--- backend/src/layers/business/todos.ts
+++ backend/src/layers/business/todos.ts
@@ -1,5 +1,7 @@
 import * as uuid from 'uuid'
 import { APIGatewayProxyEvent } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
 
 import {TodoItem} from "../../models/TodoItem"
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
@@ -9,6 +11,12 @@ import { TodosAccess } from '../data/todosAccess'
 
 const todosAccess= new TodosAccess()
 const bucketName = process.env.ATTACHMENT_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
+const XAWS = AWSXRay.captureAWS(AWS)
+const s3 = new XAWS.S3({
+  signatureVersion: 'v4'
+});
 
 export async function create(createTodoRequest: CreateTodoRequest,event: APIGatewayProxyEvent): Promise<TodoItem> {
   
@@ -33,4 +41,12 @@ export async function create(createTodoRequest: CreateTodoRequest,event: APIGate
 export async function remove(todoId){
     
     await todosAccess.remove(todoId)
+}
+
+export async function createAttachmentPresignedUrl(todoId:string): Promise<string> {
+    return s3.getSignedUrl('putObject', {
+        Bucket: bucketName,
+        Key: todoId,
+        Expires: parseInt(urlExpiration)
+      });
 }
